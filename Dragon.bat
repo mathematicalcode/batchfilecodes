f94254f069a22ba2ff8d9629b8f36e022a0b7435
mathematicalcodes <89186349+mathematicalcodes@users.noreply.github.com>
2021-08-19 13:37:42 +0530

    Add files via upload

---
+++ Dragon.bat
@@ -0,0 +1,45 @@
+@echo off
+color 0a
+title Dragon Batch
+echo DRAGON BATCH
+echo                                                         `-`      oh/       ::`                      
+echo                                                       ./yhs+:-`   `MMm+`   `ymNmy+:`                 
+echo                                  ..`                :hNMmyyyo-    mMMMm+`/ydMMMMNmmy/`              
+echo                     ``..:     `..-.     `..``      odhMmoyNMMNd:`:mmmmdddhmMMMMMMNhdNmo.            
+echo                 `...```.:.  `-.  :    ./::---.`    .++:` `+MNMmdyhdddddddNMMMMMMMMMmhNMNs.          
+echo              `..`..`   `-:---`   : `.::`-::``:-.    `     `yNMmdhhddmmmNMMMMMMMMMMMMNhdMMNo.        
+echo            `..``-.       `:..----://:   `-:.`--:`         sNMmdNMMMMMMMMMMMMMMMMMMMMMMddMMMm/       
+echo          `..  --`          --...--:-`  .-`  `./-          omMMMMMMMmMMMMMMMMMMMMMMMMMMMydMMMNs`     
+echo         ..   :.             ``````.-:` .+     `            .sNNNMMMsNMMMMMMMMMMMMMMMMMMM+NMMMMh`    
+echo       `-`   :.                  `    `` `-                   :hMMMMsoMMMNNNMmdNMMMMMMMMMdoMMMMMh`   
+echo      ..    :.                  `:      ..`                    dMMMMs.NdohmmdmNhNhsmMMMMMM:NMMMMMy   
+echo     ..    ./                   o.   .`-`                      hMMMMN.:  `hMMMMMy- `yMMNMM+hMMMMMM/  
+echo    ..     +          ---`--:` ./     :                       /MMMMMMs    ./:mMMMy   s/`-ysoMMMMMMm` 
+echo   `-     `/     `:-//`  . --+:/:     -                     :dMMMMMMmo       .NMMN        -/MMMMMMM+ 
+echo   :      :..--.`-  /`  .:.-   --     :                    -MmmmNMMM+         +Msm`        :MMMMMMMm 
+echo  ..      +:-  `-  ..  `/`-    :`     `-.                  /MhMMMMNd-         `N-/.        /MMMMMMMM-
+echo  :       +.       :`- /       ::.  `````-                 oMyMMMms``          y` `        +MMMh+sdMo
+echo `:       /        ::/.-        .-    `: -                smdyMMhh`            .           yMm:`  `+h
+echo :` .--   /        /.::          /:`   : -`               :smMNh.`                         dd.      -
+echo /.:.`-/. :`       / -/          ``--` -``-`            .oNMMhh-                          `d.        
+echo o-    `:-.-       `  .            -//` .:+-          `+mMMm+`.                           :-         
+echo .       :-/                        `-/- `.-`       `/dMMd+`                              `          
+echo          //                          -+:.  ..`    :hMNh/`                                           
+echo          `o                           ` -:. `.. `sNNs-                                              
+echo           .`                              -:. `-hMh-                                                
+echo                                             .:-oMs`                                                 
+echo                                               .My -`                                                
+echo                                               +d.: :                                                
+echo                                               h+ `:`-                                               
+echo                                              `m-  ..:                                               
+echo                                               oNs   :-                                               
+echo                                             -dmM+  :`:                                              
+echo                                             `ymMs.`-`+`                                             
+echo                                              :mNy`-`.--                                             
+echo                                              :N-:/ .-:                                             
+echo                                                os-:`./                                              
+echo                                                .so/`-`                                              
+echo                                                :.`::`                                               
+echo                                                `./::                                                
+echo                                               -:- ./                                                
+pause >nul
---
+++ converser.vbs
@@ -0,0 +1,4 @@
+dim fname
+fname=inputbox(" Hi what's your name?")
+fname=inputbox(" What's up?")
+fname=inputbox(" Thats kool.")
